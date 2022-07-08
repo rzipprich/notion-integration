10a62de8a896cab40b11bebf13aed5cfac343cb0
RZipprich Ideatek <rzipprich@ideatek.com>
2022-07-08 08:52:45 -0500

    Append to Brain Dump
    Trying to utilize the script in the Shortcuts app

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
+
+let today = new Date();
+let date = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate();
 
 const options = {
-  method: 'PATCH',
+  method: "PATCH",
   headers: {
-    'Notion-Version': '2022-06-28',
+    Accept: "application/json",
+    "Notion-Version": "2022-06-28",
     Authorization: "Bearer secret_nBE9MNaNo7sZQFILqGFy4XhA2ycy4MVsMzUBMl5qMA0",
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   },
-  body: {
+  body: JSON.stringify({
     children: [
       {
-        "heading_2": {
-          "rich_text": [
+        heading_2: {
+          rich_text: [
             {
-              "text": {
-                "content": "Lacinato kale"
-              }
-            }
-          ]
-        }
+              text: {
+                content: date,
+              },
+            },
+          ],
+        },
       },
       {
-        "paragraph": {
-          "rich_text": [
+        paragraph: {
+          rich_text: [
             {
-              "text": {
-                "content": "Lacinato kale is a variety of kale with a long tradition in Italian cuisine, especially that of Tuscany. It is also known as Tuscan kale, Italian kale, dinosaur kale, kale, flat back kale, palm tree kale, or black Tuscan palm.",
-                "link": {
-                  "url": "https://en.wikipedia.org/wiki/Lacinato_kale"
-                }
-              }
-            }
-          ]
-        }
-      }
-    ]
-  }
-}
-
+              text: {
+                content: "Enter description here",
+              },
+            },
+          ],
+        },
+      },
+    ],
+  }),
+};
 
 fetch("https://api.notion.com/v1/blocks/fed3925b90ce48969112e0e5945b9d5b/children", options)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     console.log(data);
   })
-  .catch(err => console.error(err))
+  .catch((err) => console.error(err));
